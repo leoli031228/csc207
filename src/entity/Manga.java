a38cdc5ab7d3acedb66cac21c89d7861d7effef9
lualyssa <117121956+lualyssa@users.noreply.github.com>
2023-10-30 13:42:22 -0400

    updated entities

--- src/entity/Manga.java
+++ src/entity/Manga.java
@@ -1,4 +1,35 @@
 package entity;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 public class Manga implements Media{
+    private long mangaId;
+    private String title;
+    private String genre;
+    private Date releaseDate;
+    private String coverImage;
+    private String synopsis;
+    private List<Chapter> chapters;
+
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getGenre() {
+        return null;
+    }
+
+    @Override
+    public LocalDateTime getReleaseDate() {
+        return null;
+    }
 }
