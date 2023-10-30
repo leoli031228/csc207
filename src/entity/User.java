3aa4620e532b42f5b2d6636368f1c1024e90533a
k3l5ie <kelsiefung16@gmail.com>
2023-10-30 14:07:40 -0400

    rough UML Diagram of entity classes

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,8 +1,12 @@
 package entity;
 
+import java.awt.*;
+
 public class User {
     private long userId;
     private String username;
     private String email;
     private String password;
+
+    private List<Media> watchlist;
 }
