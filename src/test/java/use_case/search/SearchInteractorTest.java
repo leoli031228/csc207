3bd31fb272b3212a2c18039d6cf0c38a9fa201dd
lualyssa <117121956+lualyssa@users.noreply.github.com>
2023-12-04 12:59:16 -0500

    started success test for search interactor using mock dao

---
+++ src/test/java/use_case/search/SearchInteractorTest.java
@@ -0,0 +1,46 @@
+package use_case.search;
+
+import data_access.MockAnimeSearchDataAccessObject;
+import entity.Anime;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SearchInteractorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    void resultsSuccessTest() {
+        SearchInputData inputTitle = new SearchInputData("Naruto");
+        SearchDataAccessInterface animeRepository = new MockAnimeSearchDataAccessObject();
+
+        SearchOutputBoundary successPresenter = new SearchOutputBoundary() {
+            @Override
+            public void prepareResultsView(SearchOutputData results) {
+                // check if output data is correct
+                Map<String, List<Object>> expectedResults = new HashMap<>();
+                expectedResults.put("Naruto", List.of("Naruto",
+                        "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg", 2));
+                assertEquals(expectedResults, results.getResults());
+            }
+
+            @Override
+            public void prepareFailView(String noResults) {
+                fail("Use case failure is unexpected");
+
+            }
+        };
+        SearchInputBoundary interactor = new SearchInteractor(animeRepository, successPresenter);
+        interactor.execute(inputTitle);
+    }
+}
