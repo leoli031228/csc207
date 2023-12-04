f7821735caa9783c49b5bc3a4ede0c12b2fba45a
davidleesaber <davidl.lee@mail.utoronto.ca>
2023-12-04 17:21:13 -0500

    updated ProgessTrackerInputBoundary and OutputBoundary

--- src/main/java/use_case/progessTracker/ProgressTrackerOutputBoundary.java
+++ src/main/java/use_case/progessTracker/ProgressTrackerOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.progessTracker;
 
+import use_case.login.LoginOutputData;
+
 public interface ProgressTrackerOutputBoundary {
+    void prepareSuccessView(ProgressTrackerOutputData results);
+
+    void prepareFailView(String noResults);
 }
