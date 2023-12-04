556d18e9afbefaf0d3735ae5f959b4981a29d706
lualyssa <117121956+lualyssa@users.noreply.github.com>
2023-12-04 03:38:17 -0500

    updates to search interface adaptor package and mock anime search dao

--- src/main/java/entity/Anime.java
+++ src/main/java/entity/Anime.java
@@ -1,20 +1,20 @@
 package entity;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Anime implements Media{
-    private long animeId;
+    private Integer animeId;
     private String title;
     private List<String> genres;
-    private Date releaseDate;
+    private LocalDateTime releaseDate;
     private String imageURL;
     private String synopsis;
-    private List<String> episodes;
+    private ArrayList<String> episodes;
 
-    Anime(long animeId, String title, List<String> genres, Date releaseDate, String imageURL, String synopsis,
-          List<String> episodes) {
+    Anime(Integer animeId, String title, List<String> genres, LocalDateTime releaseDate, String imageURL, String synopsis,
+          ArrayList<String> episodes) {
         this.animeId = animeId;
         this.title = title;
         this.genres = genres;
@@ -24,34 +24,44 @@ public class Anime implements Media{
         this.episodes = episodes;
     }
     // for mock DAO
-    public Anime(long animeId, String title) {
+    public Anime(Integer animeId, String title, String imageURL) {
         this.animeId = animeId;
         this.title = title;
+        this.imageURL = imageURL;
 
     }
 
     @Override
     public Integer getID() {
-        return null;
+        return animeId;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     @Override
     public List<String> getGenres() {
-        return null;
+        return genres;
     }
 
     @Override
     public LocalDateTime getReleaseDate() {
-        return null;
+        return releaseDate;
     }
 
     @Override
     public String getImageURL() {
-        return null;
+        return imageURL;
     }
+    @Override
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public ArrayList<String> getEpisodes() {
+        return episodes;
+    }
+
 }
