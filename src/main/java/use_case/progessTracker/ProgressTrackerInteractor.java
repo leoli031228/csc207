581c6bf841e6ee31a08a55a93f3ccf2c8f00804b
davidleesaber <davidl.lee@mail.utoronto.ca>
2023-12-04 20:04:10 -0500

    started interactor for progresschecker

--- src/main/java/use_case/progessTracker/ProgressTrackerInteractor.java
+++ src/main/java/use_case/progessTracker/ProgressTrackerInteractor.java
@@ -1,4 +1,8 @@
 package use_case.progessTracker;
 
 public class ProgressTrackerInteractor implements  ProgressTrackerInputBoundary{
+    @Override
+    public void execute(ProgressTrackerInputData inputData) {
+        return;
+    }
 }
