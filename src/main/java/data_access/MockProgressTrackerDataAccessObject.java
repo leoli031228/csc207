614d3c03265226e775374e4698720c44fb7b745d
davidleesaber <davidl.lee@mail.utoronto.ca>
2023-12-04 19:03:58 -0500

    updated mock DAO for progresschecker

--- src/main/java/data_access/MockProgressTrackerDataAccessObject.java
+++ src/main/java/data_access/MockProgressTrackerDataAccessObject.java
@@ -24,14 +24,33 @@ public class MockProgressTrackerDataAccessObject implements ProgressTrackerDataA
         animeDatabase.put("Dragon Ball", new Anime(3, "Dragon Ball",
                 "https://m.media-amazon.com/images/M/MV5BYzI0YjYxY2UtNzRjNS00NTZiLTgzMDItNGEzMjU5MmE0ZWJmXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"));
         animeDatabase.put("Attack on Titan", new Anime(4, "Attack on Titan",
-                "https://static.wikia.nocookie.net/shingekinokyojin/images/d/d8/Attack_on_Titan_Season_1.jpg/revision/latest?cb=20211005182832" ));
+                "https://static.wikia.nocookie.net/shingekinokyojin/images/d/d8/Attack_on_Titan_Season_1.jpg/revision/latest?cb=20211005182832"));
         animeDatabase.put("My Hero Academia", new Anime(5, "My Hero Academia",
                 "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/251524e3b5517b689317437d881eccf0.jpe"));
     }
 
-    public void addtoinProgress(User user, Media title) {
-        // add the media to the inProgress list and updates the statuses list in the user's profile
-        user.getInProgress.add(title);
-        user.getStatuses.put(title, "In Progress");
+    public void changeStatustoinProgress(User user, Media title) {
+        // adds the media to the inProgress list and updates the statuses list in the user's profile
+        if (!user.getInProgress.contains(title)) { // body runs only if the media is not in the progress list
+            if (user.getWatchlist.contains(title)) { // remove the media from the watch list if it is there
+                user.getWatchlist.remove(title);
+            }
+            user.getInProgress.add(title);
+            user.getStatuses.put(title, "In Progress"); // update status to in progress
+        }
+    }
+
+    public void changeStatustoFinished(User user, Media title) {
+        // if media is in Inprogress list
+        if (user.getInProgress.contains(title)) { // remove the media from the Inprogress list if it is there
+            user.getInProgress.remove(title);
+            // Since the media is in the in progress list, the media was already added to the status map
+            // so just update the status
+            user.getStatuses.replace(title, "Completed"); // update status to finished
+            return;
+        }
+        // executes this if media is not in Inprogress list, i.e. when the user finishes the media without
+        // adding it to their Inprogress list
+        user.getStatuses.put(title, "Completed");
     }
 }
