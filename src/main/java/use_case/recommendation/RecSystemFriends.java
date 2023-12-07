4580ae6efa5f6c4a3117ca8ac3e657720fc03754
GlennTheAlien <glennli2003@gmail.com>
2023-12-07 14:27:26 -0500

    revised code for RecSystemFriends

--- src/main/java/use_case/recommendation/RecSystemFriends.java
+++ src/main/java/use_case/recommendation/RecSystemFriends.java
@@ -15,42 +15,36 @@ public class RecSystemFriends implements RecSystem {
     public List<Anime> getRecommendations(User user) {
         Set<Anime> recommendedAnime = new HashSet<>();
         Set<Anime> userWatchedOrWatching = getUserAnimeList(user);
-
         for (User friend : user.getProfile().getFriends()) {
             List<Media> combinedMediaList = new ArrayList<>();
             combinedMediaList.addAll(friend.getProfile().getWatchHistory());
             combinedMediaList.addAll(friend.getProfile().getWatchlist());
             combinedMediaList.addAll(friend.getProfile().getInProgress());
-
             for (Media media : combinedMediaList) {
                 if (media instanceof Anime && !userWatchedOrWatching.contains(media)) {
                     recommendedAnime.add((Anime) media);
                 }
             }
         }
-
         return new ArrayList<>(recommendedAnime).subList(0, Math.min(recommendedAnime.size(), 5));
     }
 
     private Set<Anime> getUserAnimeList(User user) {
         Set<Anime> userAnimeList = new HashSet<>();
 
-        // Adding Anime from the user's watchHistory
-        user.getProfile().getWatchHistory().stream()
-                .filter(media -> media instanceof Anime)
-                .forEach(media -> userAnimeList.add((Anime) media));
-
-        // Adding Anime from the user's watchlist
-        user.getProfile().getWatchlist().stream()
-                .filter(media -> media instanceof Anime)
-                .forEach(media -> userAnimeList.add((Anime) media));
-
-        // Adding Anime from the user's inProgress list
-        user.getProfile().getInProgress().stream()
-                .filter(media -> media instanceof Anime)
-                .forEach(media -> userAnimeList.add((Anime) media));
+        // Process each list: watchHistory, watchlist, inProgress
+        addAnimeToList(user.getProfile().getWatchHistory(), userAnimeList);
+        addAnimeToList(user.getProfile().getWatchlist(), userAnimeList);
+        addAnimeToList(user.getProfile().getInProgress(), userAnimeList);
 
         return userAnimeList;
     }
 
+    private void addAnimeToList(List<Media> mediaList, Set<Anime> animeSet) {
+        for (Media media : mediaList) {
+            if (media instanceof Anime) {
+                animeSet.add((Anime) media);
+            }
+        }
+    }
 }
